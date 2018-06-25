91cdc4eeb318c863332b2cc9bacc8d44365e7da8
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 11:56:02 -0700

    finished writing tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const calc = require('../lib/calculator');
 const add = calc.add;
 const subtract = calc.subtract;
+const multiply = calc.multiply;
+const divide = calc.divide;
 
 describe('calculator', () => {
     it('adds two numbers', () => {
@@ -11,5 +13,13 @@ describe('calculator', () => {
     it('subtracts two numbers', () => {
         const total = subtract(3, 4);
         assert.equal(total, 1);
+    })
+    it('multiplies two numbers', () => {
+        const product = multiply(3, 4);
+        assert.equal(product, 12);
+    })
+    it('divides two numbers', () => {
+        const result = divide(12, 4);
+        assert.equal(result, 3);
     })
 });
