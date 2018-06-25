cac595157a67ffb93a6e272fdf248bfc0ec55009
Mario Quintana <pyrros2000@gmail.com>
2018-06-25 12:06:54 -0700

    adding test when diving by zero to throw an error

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,16 +10,34 @@ describe('calculator', () => {
         const sum = add(3, 4);
         assert.equal(sum, 7); 
     }); 
+
     it('subtracts two numbers', () => {
         const total = subtract(3, 4);
         assert.equal(total, 1);
     })
+
     it('multiplies two numbers', () => {
         const product = multiply(3, 4);
         assert.equal(product, 12);
     })
+
     it('divides two numbers', () => {
         const result = divide(12, 4);
         assert.equal(result, 3);
     })
+
+    it('throws error when diving by zero', () => {
+        try {
+            // run some code that we expect to throw error
+            divide(3,0);
+            // if we get here, error didn't happen
+            assert.fall('should have thrown error');
+            // (NOTE:)
+
+        }
+        catch(err) {
+
+        }
+
+    });
 });
