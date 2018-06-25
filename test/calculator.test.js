5734ca24872d1a5b9879127b5978a4a43c804ea7
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-25 11:54:51 -0700

    added mocha

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,9 +1,6 @@
 const assert = require('assert');
 const calc = require('../lib/calculator');
-const add = calc.add;
-const subtract = calc.subtract;
-const multiply = calc.multiply;
-const divide = calc.divide;
+const { add, subtract, multiply, divide } = calc;
 
 
 describe('calculator', () => {
@@ -19,12 +16,16 @@ describe('calculator', () => {
     });
 
     it('multiplies two numbers', () => {
-        const multiply = multiply(3, 4);
-        assert.equal(multiply, 12);
+        const product = multiply(3, 4);
+        assert.equal(product, 12);
     });
 
     it('divides two numbers', () => {
         const quotient = divide(4, 2);
         assert.equal(quotient, 2);
+    });
+
+    it('throws error when dividing by zero', () => {
+
     });
 });
