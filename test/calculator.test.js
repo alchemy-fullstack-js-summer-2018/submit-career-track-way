9c1c1754fb904d5279c1888d2c855df98c21c2f1
Robyn Navarro <rnavarro182@gmail.com>
2018-06-25 11:23:59 -0700

    add function test passed

---
+++ test/calculator.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const calc = require('../lib/calculator');
+const add = calc.add;
+
+describe('calculator', () => {
+    it('adds two numbers', () => {
+        const sum = add(3, 4);
+        assert.equal(sum, 7);
+    });
+});
