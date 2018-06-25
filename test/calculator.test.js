9b653ef988996c4c91c6328f0fa8d95aa72f8827
martypdx <marty@martypdx.com>
2018-06-25 11:29:29 -0700

    subtract working

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
 const calc = require('../lib/calculator');
 const add = calc.add;
+const subtract = calc.subtract;
 
 describe('calculator', () => {
 
     it('adds two numbers', () => {
         const sum = add(3, 4);
         assert.equal(sum, 7);
+    });
+
+    it('subtract two numbers', () => {
+        const difference = subtract(7, 3);
+        assert.equal(difference, 4);
     });
 });
