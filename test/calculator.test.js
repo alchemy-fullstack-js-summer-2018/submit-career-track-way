980ba7a8e795d29b765ebfeb7932e2db98be6a4b
martypdx <marty@martypdx.com>
2018-06-25 11:33:39 -0700

    multiply and divide

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const calc = require('../lib/calculator');
 const add = calc.add;
 const subtract = calc.subtract;
+const multiply = calc.multiply;
+const divide = calc.divide;
 
 describe('calculator', () => {
 
@@ -13,5 +15,15 @@ describe('calculator', () => {
     it('subtract two numbers', () => {
         const difference = subtract(7, 3);
         assert.equal(difference, 4);
+    });
+
+    it('multiplies two numbers', () => {
+        const product = multiply(3, 4);
+        assert.equal(product, 12);
+    });
+
+    it('divides two numbers', () => {
+        const quotient = divide(12, 4);
+        assert.equal(quotient, 3);
     });
 });
