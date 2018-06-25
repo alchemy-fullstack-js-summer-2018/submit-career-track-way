91e14365d19b619fd8a686ca3e7a92b0df3197da
MariahAdams <mariah.adams23@yahoo.com>
2018-06-25 12:02:39 -0700

    added catch zero case

--- test/calc.test.js
+++ test/calc.test.js
@@ -27,4 +27,17 @@ describe('calc', () => {
         const quotient = divide(8, 2);
         assert.equal(quotient, 4);
     });
+    
+    it('throws error when dividing by zero', () => {
+        try {
+            //run some code that we expect to throw err
+            divide(3, 0);
+            //if we get here, err didn't happen
+            assert.fail('Should have thrown error');
+        }
+        catch(err) {
+            //if err happens above, err should be the divide by 0 err
+            assert.equal(err.message, 'Cannot divide by zero');
+        }
+    });
 });
