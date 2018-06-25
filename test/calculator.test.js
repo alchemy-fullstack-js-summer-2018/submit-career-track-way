8eca91bd5ae57517ad332679e45046f92eb94eca
Robyn Navarro <rnavarro182@gmail.com>
2018-06-25 11:29:47 -0700

    subtract function test passed

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
 const calc = require('../lib/calculator');
 const add = calc.add;
+const subtract = calc.subtract;
 
 describe('calculator', () => {
     it('adds two numbers', () => {
         const sum = add(3, 4);
         assert.equal(sum, 7);
+    });
+
+    it('subtract two numbers', () => {
+        const result = subtract(3, 2);
+        assert.equal(result, 1);
     });
 });
