8be1a8d0971e60a5be53d6f868343074bf58a255
carriecarter <carrie@carrie-carter.com>
2018-06-25 11:54:06 -0700

    tdd passing

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,10 +1,6 @@
 const assert = require('assert');
 const calc = require('../lib/calculator');
-const add = calc.add;
-const subtract = calc.subtract;
-const multiply = calc.multiply;
-const divide = calc.divide;
-
+const { add, subtract, multiply, divide } = calc;
 
 describe('calculator', () => {
 
@@ -12,26 +8,21 @@ describe('calculator', () => {
         const sum = add(3, 4);
         assert.equal(sum, 7);
     });
-});
-
-describe('calculator', () => {
 
     it('subtracts two numbers', () => {
-        const subtract = subtract(4, 3);
-        assert.equal(subtract, 1);
+        const difference = subtract(4, 3);
+        assert.equal(difference, 1);
     });
-});
 
-describe('calculator', () => {
     it('multiplies two numbers', () => {
-        const multiply = multiply(4, 3);
-        assert.equal(multiply, 12);
+        const mult = multiply(4, 3);
+        assert.equal(mult, 12);
     });
-});
 
-describe('calculator', () => {
     it('divides two numbers', () => {
-        const divide = divide(4, 2);
+        const quotient = divide(4, 2);
         assert.equal(quotient, 2);
     });
 });
+
+
