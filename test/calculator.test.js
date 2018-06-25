e989a7993ff44d9f05e206bd5dce373b8f11c163
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-25 11:29:51 -0700

    Subtract test complete

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
 const calc = require('../lib/calculator');
 const add = calc.add;
+const subtract = calc.subtract;
 
 describe('calculator', () => {
 
     it('adds two numbers', () => {
         const sum = add(3, 4);
         assert.equal(sum, 7);
+    });
+
+    it('subtracts value Y from value X', () => {
+        const remainder = subtract(10, 3);
+        assert.equal(remainder, 7);
     });
 });
